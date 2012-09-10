de1b2797b690140da1a46dbe0e8713d346328f9e
Martin Wikström <marrewikstro@gmail.com>
2012-09-10 21:27:56 +0300

    Added the manual backend part

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ com_gservers
 ============
 
 Joomla! 2.5 component
+
+
+The GServers Joomla! component is hard coded to look for the file:
+
+http://the.website.liek/api/gservers_json.php
+
+Then you can change the "require_once" statement to load the config file
+from elsewhere since it's safer if the database login information is not
+accessible through a GET request, even if it is inside a parsed PHP-script.
---
+++ gservers_json.php
@@ -0,0 +1,57 @@
+<?php
+
+// Get the defines
+require_once("config.php");
+
+// Required defines
+//  * PREFIX_DBHOST
+//  * PREFIX_DBUSER
+//  * PREFIX_DBPASS
+//  * PREFIX_DBNAME
+
+// Connect to database
+$mysqli = new mysqli(PREFIX_DBHOST, PREFIX_DBUSER, PREFIX_DBPASS, PREFIX_DBNAME);
+if(!$mysqli) die("ERR1: Database connection failed!");
+
+// Handle sorting
+$sorting = "";
+switch(isset($_GET['sorting']) ? $_GET['sorting'] : "") {
+case "status": $sorting = " ORDER BY restartsend='no' DESC,goingdown='no' DESC"; break;
+case "game": $sorting = " ORDER BY type"; break;
+case "name": $sorting = " ORDER BY servername"; break;
+case "map": $sorting = " ORDER BY currentmap"; break;
+case "players": $sorting = " ORDER BY currentplayers DESC"; break;
+}
+
+// Handle filters
+$filter = "";
+if(isset($_GET['filter'])) if(trim($_GET['filter'])) {
+    $filter = sprintf(" WHERE type='%s'", $mysqli->real_escape_string($_GET['filter']));
+}
+
+// Query for content
+$q = "SELECT * FROM servers{$filter}{$sorting}";
+$resp = $mysqli->query($q);
+if(!$resp) die("ERR2: Query error!");
+
+// Fetch content
+$data = array();
+while($row = $resp->fetch_assoc()) {
+  $data[] = array(
+    'serverid' => $row['serverid'],
+    'servername' => $row['servername'],
+    'ip' => $row['ip'],
+    'port' => $row['port'],
+    'type' => $row['type'],
+    'currentmap' => $row['currentmap'],
+    'currentplayers' => $row['currentplayers'],
+    'maxplayers' => $row['maxplayers'],
+    'restartsend' => $row['restartsend'],
+    'goingdown' => $row['goingdown']
+  );
+}
+
+// Output content
+echo "SUCCESS:" . json_encode($data);
+
+?>
